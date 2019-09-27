4bcefcb332b011166d3ff440f888705874cf0a1b
pc-zhengchao <908665908@qq.com>
2019-09-27 13:51:46 +0800

    修改tomcat引用

--- src/main/java/com/blog/base/YcBlogApplication.java
+++ src/main/java/com/blog/base/YcBlogApplication.java
@@ -3,9 +3,16 @@ package com.blog.base;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @MapperScan("com.blog.base.dao")
 @SpringBootApplication
-public class YcBlogApplication {
+public class YcBlogApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return springApplicationBuilder.sources(YcBlogApplication.class);
+    }
     public static void main(String[] args){
         SpringApplication.run(YcBlogApplication.class);
     }
